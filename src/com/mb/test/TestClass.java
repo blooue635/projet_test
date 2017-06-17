3e60df28fb7ffd57f9ec49f557a3efd6890a7d4a
blooue635 <blooue635@gmail.com>
2017-06-17 23:42:57 +0200

    commit test

---
+++ src/com/mb/test/TestClass.java
@@ -0,0 +1,12 @@
+package com.mb.test;
+
+/**
+ * Created by Mélanie GEFFRE on 17/06/2017.
+ */
+public class TestClass {
+
+    public static void main(String[] args){
+        System.out.println("Hello World !");
+    }
+
+}
